5034c03392d687357ff9c7cc7cdfd3c0bcea6097
Bell <79753816+phpbell@users.noreply.github.com>
2021-04-13 09:42:32 -0300

    780px no container

--- theme/default/home.php
+++ theme/default/home.php
@@ -1,34 +1,20 @@
 <!DOCTYPE html>
-<html lang="pt" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <title><?php print htmlentities($site['name']); ?></title>
-    <?php
-    print $asset('css/style.css',false);
-    ?>
-</head>
-<body>
-
-</body>
-</html>
-
-<!DOCTYPE html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php print htmlentities($site['name']); ?></title>
 </head>
 <body>
-<table width="800" align="center">
+<table width="780" align="center" border="1">
 <tr>
 <td>
 <?php
 print '<center>';
 print 'hello '.$name;
+print '<p>';
+print $dbInfo['dsn'];
+print '</p>';
 print '</center>';
-print '<pre>';
-print implode('<br>',$dbInfo);
-print '</pre>';
 ?>
 </td>
 </tr>
